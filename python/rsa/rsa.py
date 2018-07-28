140809ef31124b37ff50afb0e308da526cae58e1
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-07-28 07:12:14 -0400

    Update rsa.py

--- python/rsa/rsa.py
+++ python/rsa/rsa.py
@@ -5,3 +5,7 @@ from Crypto.Hash import MD5
 
 class ersa:
 	def rsakeys():
+		length=1024
+		privatekey = RSA.generate(modulus_length, Random.new().read)
+		publickey = privatekey.publickey()
+		return privatekey, publickey
