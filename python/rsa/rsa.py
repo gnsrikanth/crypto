4ad6019eaf903fbece058ed5eb43d60df7329c6c
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-07-28 07:28:21 -0400

    Update rsa.py

--- python/rsa/rsa.py
+++ python/rsa/rsa.py
@@ -9,3 +9,7 @@ class ersa:
 		privatekey = RSA.generate(length, Random.new().read)
 		publickey = privatekey.publickey()
 		return privatekey, publickey
+	def encrypt(rsa_publickey,plain_text):
+		cipher_text=rsa_publickey.encrypt(plain_text,32)[0]
+		b64cipher=base64.b64encode(cipher_text)
+		return b64cipher
