cc14ea3507b93741c65c73048af3893543042c22
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-08-04 06:45:53 -0400

    Update simplecrypt.py

--- python/simplecrypt/simplecrypt.py
+++ python/simplecrypt/simplecrypt.py
@@ -6,9 +6,9 @@ option=sys.argv[2]
 pswd = getpass.getpass('Password:')
 if option=="encrypt":
     f=open(filename,"rb")
-    data=f.read()
+    pt=f.read()
     f.close()
-    ct=encrypt(data,pswd)
+    ct=encrypt(pswd,pt)
     f=open(filename,"wb")
     f.write(ct)
     f.close()
@@ -16,7 +16,9 @@ elif option=="decrypt":
     f=open(filename,"rb")
     ct=f.read()
     f.close()
-    pt=decrypt(ct,paswd)
+    pt=decrypt(pswd,ct)
     f=open(filename,"wb")
     f.write(pt)
     f.close()
+else:
+    print("[-]Wrong Options\n[+]Usage: python simplecrypt.py filename encrypt/decrypt")
